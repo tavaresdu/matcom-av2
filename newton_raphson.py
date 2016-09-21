0924086b958492bdac0039c302216cf1f5f8bea9
Eduardo Tavares <eduardo.tavares@cortex-intelligence.com>
2016-09-21 15:24:32 -0300

    Newton-Raphson finalizado

---
+++ newton_raphson.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+def main():
+    f = [-7.0, 22.0, -2.0] # f(x)
+    ix = 1 # Ponto X inicial
+
+    x = float(ix)
+    fd = derivada(f)
+
+    is_not_precise = True
+    while is_not_precise:
+        y = get_y(f, x)
+        yd = get_y(fd, x)
+        tn = tangente(x, y, yd)
+        xr = x_reta(tn, 0.0)
+
+        if xr == x:
+            is_not_precise = False
+        else:
+            x = xr
+
+        print xr
+
+def derivada(f):
+    g = len(f) - 1 # Grau da equação
+    fd = list() # f'(x)
+
+    for t in f: # Para cada termo da função
+        fd.append(float(t*g)) # a*X^n -> n*a*X^(n-1)
+        g = g - 1
+
+    del fd[-1] # Derivada da constante ficou 0
+    return fd
+
+def get_y(f, x):
+    g = len(f) - 1
+    y = float()
+
+    for t in f: # Para cada termo da função
+        y += t*(x**g)
+        g = g - 1
+
+    return y
+
+def tangente(x, y, yd):
+    tn = list()
+
+    tn.append(yd)
+    tn.append((yd * x * -1) + y)
+
+    return tn
+
+def x_reta(f, y):
+    xr = y + (f[1] * -1) / f[0]
+    return xr
+
+if __name__ == '__main__':
+    main()
